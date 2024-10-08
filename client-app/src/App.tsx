15a618c234f5cb7d3903e5fc5c1c716073198603
Vishnu <vishnuvardhanrao.guruvannagari@slu.edu.com>
2024-10-08 10:21:27 -0500

    Added Login essentials frontend

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage'; // Make sure the path is correct
 import RegisterStudent from './components/RegisterStudent'; // Adjust the path as necessary
 import './App.css';
+import LoginRegisterPage from './components/LoginRegisterPage';
+import LandingPage from './components/LandingPage';
+import ForgotPasswordPage from './components/ForgotPasswordPage';
 import WebcamComponent from './components/Webcam';
 
 function App() {
@@ -11,8 +14,11 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/registerstudent" element={<RegisterStudent />} />
+          <Route path="/loginregisterpage" element={<LoginRegisterPage />} />
+          <Route path="/forgotpasswordpage" element={<ForgotPasswordPage />} />
           <Route path="/capture" element={<WebcamComponent />} />
         </Routes>
       </div>
